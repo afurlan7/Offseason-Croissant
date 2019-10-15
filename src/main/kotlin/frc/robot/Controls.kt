5921e1efc8f264f948f5d8bdeada75751d512f4c
afurlan7 <55670341+afurlan7@users.noreply.github.com>
2019-10-15 11:52:37 -0700

    edits to make semi-functional
    change triggers to fix

--- src/main/kotlin/frc/robot/Controls.kt
+++ src/main/kotlin/frc/robot/Controls.kt
@@ -36,81 +36,26 @@ object Controls : Updatable {
     val driverFalconXbox = driverControllerLowLevel.mapControls {
         registerEmergencyMode()
 
-
-//        button(kB).changeOn { isClimbing = true }
-//        button(kX).changeOn { isClimbing = false }
-//        button(kA).changeOn(ClimbSubsystem.fullS3ndClimbCommand)
-        button(kX).changeOn { DriveSubsystem.lowGear = true }.changeOff { DriveSubsystem.lowGear = false }
-
-        // button(kX).changeOn(BottomRocketRoutine2())
-//        button(kX).changeOn(CharacterizationCommand(DriveSubsystem))
-        // Vision align
-//            triggerAxisButton(GenericHID.Hand.kRight).change(
-//                    ConditionalCommand(VisionDriveCommand(true), VisionDriveCommand(false),
-      //  button(kA).change(VisionDriveCommand(true))
-        //triggerAxisButton(GenericHID.Hand.kRight)
-//                            BooleanSupplier { !Superstructure.currentState.isPassedThrough }))
-        /* button(9)whileOn {
-                 button(kB).changeOn(ClimbSubsystem.hab3ClimbCommand)
-                 button(kB).changeOn(ClimbSubsystem.hab3prepMove).changeOn {isClimbing = true}
-                 triggerAxisButton(GenericHID.Hand.kLeft).changeOn(Superstructure.kCargoIntake(true)
-             }
-
-             */
-        /*// Shifting
-        if (Constants.kIsRocketLeague) {
-            button(kBumperRight).change(VisionDriveCommand(true))
-//                button(kBumperRight).change(ClosedLoopVisionDriveCommand(true))
-            button(9).changeOn { DriveSubsystem.lowGear = true }.changeOff { DriveSubsystem.lowGear = false }
-        } else {
-            triggerAxisButton(GenericHID.Hand.kRight).change(VisionDriveCommand(true))
-//                triggerAxisButton(GenericHID.Hand.kRight).change(ClosedLoopVisionDriveCommand(true))
-            button(kBumperLeft).changeOn { DriveSubsystem.lowGear = true }.changeOff { DriveSubsystem.lowGear = false }
-        } */
-//            button(kB).changeOn(ClimbSubsystem.prepMove)
-        state({ isClimbing }) {
-            pov(0).changeOn(ClimbSubsystem.hab3ClimbCommand)
-        }
-        pov(90).changeOn(ClimbSubsystem.hab3prepMove).changeOn { isClimbing = true }
-
         state({ driverControllerLowLevel.getRawButton(9) }) {
+
             state({ driverControllerLowLevel.getRawButton((10)) }) {
-                button(kBumperLeft).change(IntakeHatchCommand(releasing = true))
-                button(kBumperRight).change(IntakeHatchCommand(releasing = false))
+                //button(kBumperLeft).change(IntakeHatchCommand(releasing = true))
+
+//                button(kBumperLeft).changeOn(Superstructure.kHatchLow).changeOff { Superstructure.kStowed.schedule() }
 
                 state({ driverControllerLowLevel.getBumper(GenericHID.Hand.kRight) }) {
-                    triggerAxisButton(GenericHID.Hand.kRight).changeOn(Superstructure.kHatchHigh).changeOff { Superstructure.kStowed.schedule() }
+                    button(kY).changeOn(Superstructure.kHatchHigh).changeOff { Superstructure.kStowed.schedule() }
                     triggerAxisButton(GenericHID.Hand.kLeft).changeOn(Superstructure.kHatchMid).changeOff { Superstructure.kStowed.schedule() }
-                    button(kBumperRight).changeOn(Superstructure.kHatchLow).changeOff { Superstructure.kStowed.schedule() }
+                    button(kBumperLeft).changeOn(Superstructure.kHatchLow).changeOff { Superstructure.kStowed.schedule() }
                 }
-                triggerAxisButton(GenericHID.Hand.kRight).whileOn {
-                    button(kBumperLeft).changeOn(Superstructure.kCargoHigh).changeOff { Superstructure.kStowed.schedule() }
-                    button(kBumperRight).changeOn(Superstructure.kCargoMid).changeOff { Superstructure.kStowed.schedule() }
-                    triggerAxisButton(GenericHID.Hand.kLeft).changeOn(Superstructure.kCargoLow).changeOff { Superstructure.kStowed.schedule() }
-                    button(kB).changeOn(Superstructure.kCargoShip).changeOff { Superstructure.kStowed.schedule() }
-                }
-//            }
+                /* state({ operatorXboxController.getBumper(GenericHID.Hand.kLeft) }) {
+                     button(kY).changeOn(Superstructure.kCargoHigh).changeOff { Superstructure.kStowed.schedule() }
+                     button(kBumperRight).changeOn(Superstructure.kCargoMid).changeOff { Superstructure.kStowed.schedule() }
+                     triggerAxisButton(GenericHID.Hand.kLeft).changeOn(Superstructure.kCargoLow).changeOff { Superstructure.kStowed.schedule() }
+                     button(kB).changeOn(Superstructure.kCargoShip).changeOff { Superstructure.kStowed.schedule() }
+                 } */
+            }
         }
-
-////        button(9).whileOn {
-////            button(10).whileOn {
-//                button(kBumperLeft).change(IntakeHatchCommand(releasing = true))
-//                button(kBumperRight).change(IntakeHatchCommand(releasing = false))
-//
-//                while (!IntakeHatchCommand(releasing = false).isFinished()) {
-//                    triggerAxisButton(GenericHID.Hand.kRight).changeOn(Superstructure.kHatchHigh).changeOff { Superstructure.kStowed.schedule() }
-//                    triggerAxisButton(GenericHID.Hand.kLeft).changeOn(Superstructure.kHatchMid).changeOff { Superstructure.kStowed.schedule() }
-//                    button(kBumperRight).changeOn(Superstructure.kHatchLow).changeOff { Superstructure.kStowed.schedule() }
-//                }
-//                triggerAxisButton(GenericHID.Hand.kRight).whileOn {
-//                    button(kBumperLeft).changeOn(Superstructure.kCargoHigh).changeOff { Superstructure.kStowed.schedule() }
-//                    button(kBumperRight).changeOn(Superstructure.kCargoMid).changeOff { Superstructure.kStowed.schedule() }
-//                    triggerAxisButton(GenericHID.Hand.kLeft).changeOn(Superstructure.kCargoLow).changeOff { Superstructure.kStowed.schedule() }
-//                    button(kB).changeOn(Superstructure.kCargoShip).changeOff { Superstructure.kStowed.schedule() }
-////                }
-//            }
-        }
-    }
 
 
 }
@@ -120,32 +65,49 @@ object Controls : Updatable {
 //        button(kY).changeOn(ClimbSubsystem.fullS3ndClimbCommand)
 //    }
 
-   // val operatorXboxController = XboxController(0)
-  //  val operatorJoy = Joystick (5)
+val operatorXboxController = XboxController(5)
+val operatorFalconHID = operatorXboxController.mapControls {
+    //  val operatorJoy = Joystick (5)
     // val operatorFalconHID = operatorJoy.mapControls {
-//val operatorFalconHID = operatorXboxController.mapControls {
-        // intake hatch
+    state({ driverControllerLowLevel.getRawButton(9) }) {
+        state({ driverControllerLowLevel.getRawButton((10)) }) {
+            //button(kBumperLeft).change(IntakeHatchCommand(releasing = true))
 
-        // intake ball
-        // cargo -- intake is a bit tricky, it'll go to the intake preset automatically
-        // the lessThanAxisButton represents "intaking", and the greaterThanAxisButton represents "outtaking"
-        /*val cargoCommand = sequential {
-            +PrintCommand("running cargoCommand")
-            +Superstructure.kCargoIntake
-            +IntakeCargoCommand(releasing = false)
+
+            state({ operatorXboxController.getBumper(GenericHID.Hand.kRight) }) {
+                triggerAxisButton(GenericHID.Hand.kRight).changeOn(Superstructure.kHatchHigh).changeOff { Superstructure.kStowed.schedule() }
+                triggerAxisButton(GenericHID.Hand.kLeft).changeOn(Superstructure.kHatchMid).changeOff { Superstructure.kStowed.schedule() }
+                button(kBumperLeft).changeOn(Superstructure.kHatchLow).changeOff { Superstructure.kStowed.schedule() }
+            }
+           /* state({ operatorXboxController.getBumper(GenericHID.Hand.kLeft) }) {
+                button(kY).changeOn(Superstructure.kCargoHigh).changeOff { Superstructure.kStowed.schedule() }
+                button(kBumperRight).changeOn(Superstructure.kCargoMid).changeOff { Superstructure.kStowed.schedule() }
+                triggerAxisButton(GenericHID.Hand.kLeft).changeOn(Superstructure.kCargoLow).changeOff { Superstructure.kStowed.schedule() }
+                button(kB).changeOn(Superstructure.kCargoShip).changeOff { Superstructure.kStowed.schedule() }
+            } */
         }
-        triggerAxisButton(GenericHID.Hand.kLeft).changeOff {
-            (sequential { +ClosedLoopWristMove(40.degree) ; +Superstructure.kStowed; }).schedule()
-        }.change(cargoCommand)
+    }
+    // intake hatch
+
+    // intake ball
+    // cargo -- intake is a bit tricky, it'll go to the intake preset automatically
+    // the lessThanAxisButton represents "intaking", and the greaterThanAxisButton represents "outtaking"
+    val cargoCommand = sequential {
+        +PrintCommand("running cargoCommand")
+        +Superstructure.kCargoIntake
+        +IntakeCargoCommand(releasing = false)
+    }
+    triggerAxisButton(GenericHID.Hand.kLeft).changeOff {
+        (sequential { +ClosedLoopWristMove(40.degree); +Superstructure.kStowed; }).schedule()
+    }.change(cargoCommand)
 
 
+    val cargoRelease = sequential {
+        +PrintCommand("running cargoRelease")
+        +Superstructure.kCargoIntake
+        +IntakeCargoCommand(releasing = true)
+    }
 
-        val cargoRelease = sequential {
-            +PrintCommand("running cargoRelease")
-            +Superstructure.kCargoIntake
-            +IntakeCargoCommand(releasing = true)
-        }
-*/
 /*      **** OLD JOYSTICK CODE *****
         // climbing
 
@@ -189,18 +151,18 @@ object Controls : Updatable {
 */
 
 
+}
 
 
+    private fun Command.andThen(block: () -> Unit) = sequential { +this@andThen; +InstantCommand(Runnable(block)) }
 
-
-
-private fun Command.andThen(block: () -> Unit) = sequential { +this@andThen ; +InstantCommand(Runnable(block)) }
-
-private fun FalconXboxBuilder.registerEmergencyMode() {
-    button(kBack).changeOn {
-        Robot.activateEmergency()
+    private fun FalconXboxBuilder.registerEmergencyMode() {
+        button(kBack).changeOn {
+            Robot.activateEmergency()
+        }
+        button(kStart).changeOn {
+            Robot.recoverFromEmergency()
+        }
     }
-    button(kStart).changeOn {
-        Robot.recoverFromEmergency()
-    }
+
 }
